e49bb3589648c9812a2f6aca948a888411ed7240
LeoBang <bluke8489@gmail.com>
2021-09-28 17:47:18 +0900

    feat: jwt token

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,6 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
@@ -6,6 +8,16 @@ import { UserRepository } from './user.repository';
 
 @Module({
   imports: [
+    // passport module이 기본적으로 이용하는 것은 jwt라고 명시해주자.
+    PassportModule.register({ defaultStrategy: "jwt" }),
+    JwtModule.register({
+      // server에서 저장되는 secret key는 임의로 정해주면됨.
+      secret: "Secret1234",
+      signOptions: {
+        // 언제 토큰이 만료되는지. -> 3600분 이후에 만료
+        expiresIn: 3600 * 60
+      }
+    }),
     TypeOrmModule.forFeature([UserRepository])
   ],
   controllers: [AuthController],
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,16 +1,38 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { AuthCredentialDto } from './dto/auth-credential.dto';
 import { UserRepository } from './user.repository';
+import * as bcrypt from "bcryptjs";
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
     constructor(
         @InjectRepository(UserRepository)
-        private userRepository: UserRepository
+        private userRepository: UserRepository,
+        private jwtService: JwtService
     ) {}
 
     signUp(authCredentialDto: AuthCredentialDto): Promise<void> {
         return this.userRepository.signUp(authCredentialDto);
+    }
+
+    async signIn(authCredentialDto: AuthCredentialDto): Promise<{accessToken: string, message: string}> {
+        const {username, password } = authCredentialDto;
+        const user = await this.userRepository.findOne({ username: username });
+
+        if (user && (await bcrypt.compare(password, user.password))) {
+            // user token 생성 (Secret + Payload가 필요)
+            const payload = {
+                // user이름, user의 역할, user 이메일 등 많이 넣어줄 수 있음
+                // 중요한 정보는 넣어두면 안된다.
+                username : username
+            }
+            const accessToken = await this.jwtService.sign(payload);
+            
+            return { accessToken : accessToken, message: "login success"};
+        } else {
+            throw new UnauthorizedException("login failed");
+        }
     }
 }
